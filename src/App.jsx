f77cc9b981ec916df67db5fba55e2735532d876e
olgaKhristo <olga84h@gmail.com>
2023-04-20 16:39:19 +0100

    firsr code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+export default function App() {
+  return <form className = "new-item-form">
+<div className="form-row">
+<label htmlFor = "item">New Item</label>
+<input type = "text" id = "item"/>  
+</div>
+  </form>
+}
